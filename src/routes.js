e1735b422aef4871943cb7205a707c19075ec4e6
yvpaulo@yahoo.com.br <yvpaulo@yahoo.com.br>
2019-07-05 10:13:28 -0300

    criacao do  providercontroller

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -19,6 +20,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/providers', ProviderController.index);
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Olá Mestre Yvson' });
