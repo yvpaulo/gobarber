a1d119c932b693767f052a2d6809307649d67b9d
yvpaulo@yahoo.com.br <yvpaulo@yahoo.com.br>
2019-07-17 17:43:18 -0300

    Finalização da api

--- src/config/database.js
+++ src/config/database.js
@@ -12,3 +12,20 @@ module.exports = {
     underscoredAll: true,
   },
 };
+
+/**
+ *
+module.exports = {
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
+
+ */
